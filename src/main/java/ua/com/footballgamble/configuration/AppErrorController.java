a6a1ee98c07458a6e6ecafb9831c70355f6abec3
AnGo84 <ango1984@gmail.com>
2019-09-07 01:32:31 +0300

    Added resources to pom

--- src/main/java/ua/com/footballgamble/configuration/AppErrorController.java
+++ src/main/java/ua/com/footballgamble/configuration/AppErrorController.java
@@ -1,9 +1,12 @@
 package ua.com.footballgamble.configuration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import ua.com.footballgamble.contloller.CompetitionController;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.PrintWriter;
@@ -11,13 +14,14 @@ import java.io.StringWriter;
 
 @Controller
 public class AppErrorController implements ErrorController {
-
+	public static final Logger logger = LoggerFactory.getLogger(AppErrorController.class);
 	@RequestMapping("/error")
 	@ResponseBody
 	public String handleError(HttpServletRequest request) {
+
 		Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
 		Exception exception = (Exception) request.getAttribute("javax.servlet.error.exception");
-
+		logger.error("Exception message: " + exception.getMessage(),exception);
 		String exceptionStackTrace = getExceptionStackTrace(exception);
 
 		return String.format("<html><body><h2>Error Page</h2><div>Status code: <b>%s</b></div>"
